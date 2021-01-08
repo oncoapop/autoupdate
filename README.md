4587efaa25d7ae8add221c10826903c2ed900113
oncoapop <oncoapop@sdf.org>
2021-01-08 14:51:32 -0800

    Start with the autoupdate scripts

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Script to enable automatic server upgrades
 
 # Implementation of 
 https://phoenixnap.com/kb/automatic-security-updates-ubuntu
+https://help.ubuntu.com/18.04/serverguide/automatic-updates.html
+
+
---
+++ setup_autoupdates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Implementation
+# https://help.ubuntu.com/18.04/serverguide/automatic-updates.html
+
+# Copy git info
+scp oncoapop@ma.sdf.org:~/.netrc .
+git clone https://github.com/oncoapop/autoupdate.git
+
+sudo apt install unattended-upgrades
+systemctl status unattended-upgrades
+
+sudo cp unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+sudo cp auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+
+# sudo unattended-upgrades
